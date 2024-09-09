a752c2c0b58480a2980180a8597108b06ca33898
wendy <mizukilee@gmail.com>
2024-09-08 23:03:31 -0400

    passing json

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
-import './App.css'
-import  { Greet } from './components/Greet'
+import './App.css'; 
+import { Greet } from './components/Greet'; 
+import { Person } from './components/Person'; 
 
 function App() {
+
+  //personName is passed to Person component as a prop 
+  const personName = { 
+    first: 'Bruce', 
+    last: 'Wayne'
+  }
 
   return (
     <div className='App'> 
       <Greet name='Wendy' age={32} isAdult={true} />
+      <Person person={personName} />
+
     </div>
   )
 }
